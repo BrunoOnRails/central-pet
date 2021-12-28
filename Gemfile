6b2b502ac17dfb2818c60f3faa696df78378cf0e
brunocesarofc <brunocesar_ofc@hotmail.com>
2021-12-28 15:51:44 -0300

    adição do bootstrap e alteração na tela de cad

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.3'
 
+gem 'i18n', '~> 1.8', '>= 1.8.11'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
 # Use postgresql as the database for Active Record
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module CentralCad
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -18,5 +19,7 @@ module CentralCad
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    default_locate = "pt-BR"
+
   end
 end
